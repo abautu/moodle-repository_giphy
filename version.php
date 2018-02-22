4b22430b952852fd22f8a555068cfb7523a5f7ca
Andrei Bautu <abautu@gmail.com>
2018-02-22 15:20:44 +0200

    bump version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018012800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2018022200;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'repository_giphy'; // Full name of the plugin (used for diagnostics).
 $plugin->release   = '1.0';
